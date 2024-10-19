cbe1d5056140c2f999083d8aa5ee2ef41012bf8c
AbergerJonas <145124362+jonasaberger@users.noreply.github.com>
2024-10-19 17:25:43 +0200

    - Initialize WPF-Project

--- Ritterturnier/RitterturnierWPF/obj/Debug/net8.0-windows/RitterturnierWPF.AssemblyInfo.cs
+++ Ritterturnier/RitterturnierWPF/obj/Debug/net8.0-windows/RitterturnierWPF.AssemblyInfo.cs
@@ -11,13 +11,15 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("RitterturnierXUNIT")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("RitterturnierWPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc44854dac28ff4228360248297a515844528d52")]
-[assembly: System.Reflection.AssemblyProductAttribute("RitterturnierXUNIT")]
-[assembly: System.Reflection.AssemblyTitleAttribute("RitterturnierXUNIT")]
+[assembly: System.Reflection.AssemblyProductAttribute("RitterturnierWPF")]
+[assembly: System.Reflection.AssemblyTitleAttribute("RitterturnierWPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
 
