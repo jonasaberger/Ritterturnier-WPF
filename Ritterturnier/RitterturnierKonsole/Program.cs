73f99b0589cceaad4058a88c3ba3cc6c40d19d15
AbergerJonas <145124362+jonasaberger@users.noreply.github.com>
2024-10-19 15:25:26 +0200

    -Create Console Application + Added Documentation

---
+++ Ritterturnier/RitterturnierKonsole/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RitterturnierKonsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ritterturnier-Konsole\nJonas Aberger\t 19-10-2024\n\n");
+
+
+        }
+    }
+}
